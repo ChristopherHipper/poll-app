3a68e7a1eaa1c792588e36567f4f0085335b86cb
Christopher Hipper <hipper.christopher@gmail.com>
2026-05-30 14:49:21 +0200

    update: survey form

--- src/app/pages/survey-form/survey-form.ts
+++ src/app/pages/survey-form/survey-form.ts
@@ -1,24 +1,32 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Logo } from "../../shared/logo/logo";
 import { RouterLink } from "@angular/router";
 import { CancelButton } from "../../shared/buttons/cancel-button/cancel-button";
 import { Status } from "../../shared/status/status";
-import { FormBuilder, Validators , ReactiveFormsModule} from '@angular/forms';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Dropdown } from "../../shared/dropdown/dropdown";
+import { ClearButton } from "../../shared/buttons/clear-button/clear-button";
+import { Question } from '../../shared/interface/question';
 
 @Component({
   selector: 'app-survey-form',
-  imports: [Logo, RouterLink, CancelButton, Status, ReactiveFormsModule, Dropdown],
+  imports: [Logo, RouterLink, CancelButton, Status, ReactiveFormsModule, Dropdown, ClearButton],
   templateUrl: './survey-form.html',
   styleUrl: './survey-form.scss',
 })
 export class SurveyForm {
   formbuilder = inject(FormBuilder);
   showSuccessMessage = signal(false);
+  questions = signal<Question[]>([
+    {
+      id: 0,
+      title: '',
+    },
+  ]);
 
   surveyForm = this.formbuilder.group({
     name: ['', [Validators.required, Validators.minLength(1)]],
-    message: ['', []],
+    description: ['', []],
     date: ['', []],
   });
 
@@ -29,6 +37,30 @@ export class SurveyForm {
 
   formSubmit() { }
 
-  getCategory(value: string){}
+  getCategory(value: string) { }
+
+  clearField(controlName: keyof typeof this.surveyForm.controls) {
+    this.surveyForm.controls[controlName].setValue('')
+  }
+
+  increaseQuestions() {
+
+  }
+
+  removeQuestion(questionIndex: number) {
+    if (questionIndex == 0) {
+      console.log('clear erste frage');
+
+    } else {
+      this.deleteQuestion(questionIndex)
+      console.log('lösche frage an der stelle', questionIndex);
+    }
+  }
+
+  deleteQuestion(index: number) {
+    this.questions.update(questions => questions.filter(question => question.id === index));
+  }
 
 }
+
+
---
+++ src/app/shared/buttons/clear-button/clear-button.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clear-button',
+  imports: [],
+  templateUrl: './clear-button.html',
+  styleUrl: './clear-button.scss',
+})
+export class ClearButton {
+
+}
