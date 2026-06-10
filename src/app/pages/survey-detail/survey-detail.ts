0a37d26666b96de5951c6b214fdc2c9d7d9f0dd3
Christopher Hipper <hipper.christopher@gmail.com>
2026-06-10 21:27:02 +0200

    feat: detail live view and implement live counter for selected answers

--- src/app/pages/survey-detail/survey-detail.ts
+++ src/app/pages/survey-detail/survey-detail.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Logo } from "../../shared/logo/logo";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { Status } from "../../shared/status/status";
 import { Surveys } from '../../shared/service/surveys';
 import { DatePipe } from '@angular/common';
-
-import { FormBuilder, Validators, ReactiveFormsModule, FormArray, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Answer } from '../../shared/interface/answer';
 
 @Component({
   selector: 'app-survey-detail',
@@ -14,24 +14,56 @@ import { FormBuilder, Validators, ReactiveFormsModule, FormArray, FormGroup } fr
   styleUrl: './survey-detail.scss',
 })
 export class SurveyDetail {
-  router = inject(Router)
+  router = inject(Router);
   private route = inject(ActivatedRoute);
-  surveyService = inject(Surveys)
-  singleSurvey = this.surveyService.singleSurvey
+  surveyService = inject(Surveys);
+  singleSurvey = this.surveyService.singleSurvey;
+  currentAnswerId = signal(0);
+
 
   ngOnInit() {
     const currentid = this.route.snapshot.paramMap.get('id');
     if (!currentid) return;
-    this.surveyService.getSurveyById(+currentid)
+    this.surveyService.getSurveyById(+currentid);
   };
 
   getAnswerLabel(index: number): string {
     return String.fromCharCode(65 + index);
   };
 
-  updateSurvey(){
+  getPercentage(votes: number, answers: Answer[]): number {
+    const percentage = 100 * votes / this.getAllVotes(answers);
+    return Math.round(percentage);
+  };
+
+  getAllVotes(answers: Answer[]): number {
+    let allVotes = 0;
+    answers.forEach((a) => [
+      allVotes += a.votes
+    ]);
+    return allVotes;
+  };
+
+  updateVotes(event: Event, answer: Answer, answers: Answer[]) {
+    const input = (event.target as HTMLInputElement)
+    if (input.type == 'checkbox') {
+      answer.votes += input.checked ? 1 : -1;
+      return;
+    } else {
+      if (answer.id != this.currentAnswerId()) {
+        answer.votes++;
+        answers.forEach((a) => {
+          if (a.id === this.currentAnswerId()) {
+            a.votes--
+          };
+        });
+        this.currentAnswerId.set(answer.id);
+      };
+    };
+  };
+
+  updateSurvey() {
     console.log('update');
-    
-  }
+  };
 
 }
